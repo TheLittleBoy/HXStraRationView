dc541383c1061b138d9341cc4cbbe520283e99b2
TheLittleBoy <813608245@qq.com>
2015-02-08 19:42:11 +0800

    HXStarRatingView
    继承自UIView

---
+++ HXStraRationView/HXStarRatingView/HXStarRatingView.h
@@ -0,0 +1,13 @@
+//
+//  HXStarRatingView.h
+//  HXStraRationView
+//
+//  Created by MAC_AO on 15/2/8.
+//  Copyright (c) 2015年 MAC_AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXStarRatingView : UIView
+
+@end
---
+++ HXStraRationView/HXStarRatingView/HXStarRatingView.m
@@ -0,0 +1,21 @@
+//
+//  HXStarRatingView.m
+//  HXStraRationView
+//
+//  Created by MAC_AO on 15/2/8.
+//  Copyright (c) 2015年 MAC_AO. All rights reserved.
+//
+
+#import "HXStarRatingView.h"
+
+@implementation HXStarRatingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
